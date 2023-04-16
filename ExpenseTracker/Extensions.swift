b044157914914f65d2361d4f2a91f342bd063f56
Dyan <lumbar_plaice0i@icloud.com>
2023-04-16 15:04:43 -0300

    feat: color to project

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Dyan on 4/16/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
