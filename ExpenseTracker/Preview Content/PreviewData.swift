4ec7b661c384aa16e9786c490fd4185d7fa42b15
Dyan <lumbar_plaice0i@icloud.com>
2023-04-16 16:45:49 -0300

    feat: leyout, format date, add icons

---
+++ ExpenseTracker/Preview Content/PreviewData.swift
@@ -0,0 +1,11 @@
+//
+//  PreviewData.swift
+//  ExpenseTracker
+//
+//  Created by Dyan on 4/16/23.
+//
+
+import Foundation
+var transactionPreviewData = Transaction(id: 1, date: "91/24/2022", institution: "Desjardins", account: "Visa Desjardins",merchant: "Apple", amount: 11.49, type: "debit", categoryId: 801, category: "Software", isPending: false, isTransfer: false, isExpense: true, isEdited: false)
+
+var transactionListPreviewData = [Transaction](repeating: transactionPreviewData, count: 10)
